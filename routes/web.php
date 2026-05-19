c563a3934bfdd49dad13e80b65afbbee526587a8
Farel Rizantha <farelrizantha@gmail.com>
2026-05-19 23:41:52 +0700

    Pertemuan 9

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DosenController;
 use App\Http\Controllers\PageController;
+use App\Http\Controllers\PegawaiController ;
+use App\Http\Controllers\BlogController ;
 
 Route::get('/', function () {
     return view('welcome');
@@ -32,3 +34,13 @@ Route::get('pertemuan4', [PageController::class, 'pertemuan4']);
 Route::get('pertemuan5', [PageController::class, 'pertemuan5']);
 Route::get('responsive', [PageController::class, 'responsive']);
 Route::get('template', [PageController::class, 'template']);
+
+
+
+Route::get('/pegawai/{nama}', [PegawaiController::class, 'index']);
+Route::get('/formulir', [PegawaiController::class, 'formulir']);
+Route::post('/formulir/proses', [PegawaiController::class, 'proses']);
+//blog
+Route::get('/blog', [BlogController::class, 'home']);
+Route::get('/blog/tentang', [BlogController::class, 'tentang']);
+Route::get('/blog/kontak', [BlogController::class, 'kontak']);
